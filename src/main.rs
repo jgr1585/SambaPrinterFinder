d0b19cdbc7e98d6f9d78e4dd20ad80ebd99b531f
jgr1585 <jgr1585@students.fhv.at>
2026-02-05 16:07:40 +0100

    Set application ID and configure default icon in main function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
+use std::path::Path;
+use gtk::gdk::Display;
+use gtk::{Application, IconTheme, Window};
 use gtk::prelude::*;
 use samba_printer_finder::gui::build_ui;
 
 fn main() -> glib::ExitCode {
-    let application = gtk::Application::builder()
-        .application_id("at.jeb.samba_printer_finder")
+    let application = Application::builder()
+        .application_id("io.github.jgr1585.SambaPrinterFinder")
         .build();
+
+    // Set default application icon
+    application.connect_startup(|_app| {
+        if let Some(display) = Display::default() {
+            let icon_theme = IconTheme::for_display(&display);
+            let icon_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("assets");
+
+            if let Some(path) = icon_path.to_str() {
+                icon_theme.add_search_path(path);
+                Window::set_default_icon_name("icon");
+            }
+        }
+    });
 
     application.connect_activate(build_ui);
     application.run()
